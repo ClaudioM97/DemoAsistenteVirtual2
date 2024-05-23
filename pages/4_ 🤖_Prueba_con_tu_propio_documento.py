7b87df1e894593bae8108506b996e07149ddc0f0
ClaudioMontiel <cmontiel@brainfood.cl>
2024-05-23 11:47:04 -0400

    aa

--- pages/4_ 🤖_Prueba_con_tu_propio_documento.py
+++ pages/4_ 🤖_Prueba_con_tu_propio_documento.py
@@ -61,6 +61,9 @@ st.markdown('''En este apartado podrás cargar tu propio documento en formato PD
 
 uploaded_file = st.file_uploader("Carga tu archivo PDF", type="pdf")
 
+if uploaded_file is None:
+    st.write('Lo lamento, tienes que cargar un documento PDF primero ☹️')
+
 if uploaded_file is not None:
      with st.spinner("Procesando..."):
         
